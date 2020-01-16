4a31a82aeffb4c86dc79927b7f78072babd3fbec
Sean <srbuck1111@gmail.com>
2020-01-15 19:11:38 -0500

    initial commit

---
+++ DndProject/src/main/java/co/srbuck1111/DndProject/DndProjectApplication.java
@@ -0,0 +1,13 @@
+package co.srbuck1111.DndProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndProjectApplication.class, args);
+	}
+
+}
---
+++ DndProject/src/test/java/co/srbuck1111/DndProject/DndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.srbuck1111.DndProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
